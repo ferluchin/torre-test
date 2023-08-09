3c78b7cfbde063d05889f947fd94b6fc3adeb953
Luis Granda <lgrandab@gmail.com>
2023-08-09 15:02:09 -0500

    added work and education experiences

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,51 +1,59 @@
 // filename: Experiences.js
 
 import React, { useContext } from "react";
-import './Experiences.css';
-// Assuming you're importing the same context
+import "./Experiences.css";
 import { AppContext } from "../context/AppContext";
 
 const Experiences = () => {
     const { data } = useContext(AppContext);
 
+    const jobExperiences =
+        data?.experiences?.filter((exp) => exp.category === "jobs") || [];
+    const educationExperiences =
+        data?.experiences?.filter((exp) => exp.category === "education") || [];
+
     return (
         <div className="experiences">
-            <h2>Experiences</h2>
-
-            {data?.experiences?.map((experience, index) => (
-                <div key={index} className="experience">
-                    <h3>{experience.name}</h3>
-
-                    {experience.organizations.map((org, orgIndex) => (
-                        <div key={orgIndex}>
-                            <img src={org.picture} alt={org.name} />
-                            <p>{org.name}</p>
-                        </div>
-                    ))}
-
-                    <h4>Responsibilities:</h4>
-                    <ul>
-                        {experience.responsibilities.map(
-                            (responsibility, resIndex) => (
-                                <li key={resIndex}>{responsibility}</li>
-                            )
-                        )}
-                    </ul>
-
-                    <p>
-                        From: {experience.fromMonth} {experience.fromYear}
-                    </p>
-                    {experience.toMonth && (
-                        <p>
-                            To: {experience.toMonth} {experience.toYear}
-                        </p>
-                    )}
-
-                    {/* Add more fields as required */}
-                </div>
-            ))}
+            <section className="work-experiences">
+                <h2>Job Experiences</h2>
+                {renderExperiences(jobExperiences)}
+            </section>
+            <section className="education-experiences">
+                <h2>Education</h2>
+                {renderExperiences(educationExperiences)}
+            </section>
         </div>
     );
+};
+
+const renderExperiences = (experiences) => {
+    return experiences.map((experience, index) => (
+        <div key={index} className="experience">
+            <h3>{experience.name}</h3>
+            {experience.organizations.map((org, orgIndex) => (
+                <div key={orgIndex}>
+                    {org.picture ? (
+                        <img src={org.picture} alt={org.name} />
+                    ) : (
+                        <div className="placeholderIcon">⭐</div>
+                    )}
+                    <p>{org.name}</p>
+                </div>
+            ))}
+            <h4>Responsibilities:</h4>
+            <ul>
+                {experience.responsibilities.map((responsibility, resIndex) => (
+                    <li key={resIndex}>{responsibility}</li>
+                ))}
+            </ul>
+            <p>
+                From: {experience.fromMonth} {experience.fromYear} to
+                {experience.toMonth
+                    ? ` ${experience.toMonth} ${experience.toYear}`
+                    : " Present"}
+            </p>
+        </div>
+    ));
 };
 
 export default Experiences;
